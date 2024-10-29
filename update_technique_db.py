366bc521332664bc9d68f3e36a60c9d0fe57741a
Darkiros <romain.loisel@aol.fr>
2024-10-29 09:09:30 +0100

    Add suport of Mobile & ICS MITRE ATT&CK layer (#2)

--- update_technique_db.py
+++ update_technique_db.py
@@ -1,16 +1,21 @@
 import json
 import pandas as panda
 
-TECHNIQUES_FILE_URL = "https://attack.mitre.org/docs/enterprise-attack-v15.1/enterprise-attack-v15.1-techniques.xlsx"
+TECHNIQUES_ENTERPRISE_FILE_URL = "https://attack.mitre.org/docs/enterprise-attack-v15.1/enterprise-attack-v15.1-techniques.xlsx"
+ENTERPRISE_XSLX_CASE = 9
+TECHNIQUES_MOBILE_FILE_URL = "https://attack.mitre.org/docs/mobile-attack-v15.1/mobile-attack-v15.1-techniques.xlsx"
+MOBILE_XSLX_CASE = 10
+TECHNIQUES_ICS_FILE_URL = "https://attack.mitre.org/docs/ics-attack-v15.1/ics-attack-v15.1-techniques.xlsx"
+ICS_XSLX_CASE = 9
 TECHNIQUES_FILE = "resources/techniques_db.json"
 
 # Download the techniques data
-def download_techniques():
+def download_techniques(base_url, case):
     try:
-        data = panda.read_excel(TECHNIQUES_FILE_URL)
+        data = panda.read_excel(base_url)
         result = {}
         for i in range(0, len(data)):
-            result[data.iloc[i, 0]] = data.iloc[i, 9].split(", ")
+            result[data.iloc[i, 0]] = data.iloc[i, case].split(", ")
         return result
     except Exception as e:
         print(f"Error downloading the data: {str(e)}")
@@ -25,7 +30,9 @@ def save_json(data):
 
 if __name__ == "__main__":
     print("[!] Downloading techniques data...")
-    techniques_data = download_techniques()
+    techniques_data = download_techniques(TECHNIQUES_ENTERPRISE_FILE_URL, ENTERPRISE_XSLX_CASE)
+    techniques_data.update(download_techniques(TECHNIQUES_MOBILE_FILE_URL, MOBILE_XSLX_CASE))
+    techniques_data.update(download_techniques(TECHNIQUES_ICS_FILE_URL, ICS_XSLX_CASE))
     if techniques_data:
         print("[!] Saving techniques data...")
         save_json(techniques_data)
